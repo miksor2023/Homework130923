939200cdfc66bf83b678d0d2e0716c34167fcaaa
miksor2023 <miksor@yandex.ru>
2023-09-13 19:41:50 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+    public static void task1() {
+        int[] arr = generateRandomArray();
+        System.out.println("\nЗадача 1");
+        Arrays.toString(arr);
+        System.out.println(arr[10]);
+        System.out.println("\nЗадача 1");
     }
 }
